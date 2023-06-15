9fc615214317a2e15d946bae229fc2020b9c5895
Abdurrahman Ziyad <abdurrahman.ziyad17@gmail.com>
2023-06-15 21:22:13 +0700

    Create RESTful API Places Web

--- routes/api.php
+++ routes/api.php
@@ -44,4 +44,13 @@ Route::prefix('web')->group(function () {
 
     //route categories show
     Route::get('/categories/{slug?}', [App\Http\Controllers\Api\Web\CategoryController::class, 'show', ['as' => 'web']]);
+
+    //route places index
+    Route::get('/places', [App\Http\Controllers\Api\Web\PlaceController::class, 'index', ['as' => 'web']]);
+
+    //route places show
+    Route::get('/places/{slug?}', [App\Http\Controllers\Api\Web\PlaceController::class, 'show', ['as' => 'web']]);
+
+    //route all places index
+    Route::get('/all_places', [App\Http\Controllers\Api\Web\PlaceController::class, 'all_places', ['as' => 'web']]);
 });
